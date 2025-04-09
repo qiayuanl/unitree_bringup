a52b03246d47ef9685e2d978f5605a7f40d9860b
qiayuanl <qiayuanl@berkeley.edu>
2025-04-08 17:16:06 -0700

    Initial commit

---
+++ launch/teleop.launch.py
@@ -0,0 +1,23 @@
+import os
+
+import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    param = os.path.join(
+        get_package_share_directory('unitree_bringup'),
+        'config', 'g1', 'joy.yaml'
+    )
+    joy_node = launch_ros.actions.Node(package='joy_linux',
+                                       executable='joy_linux_node',
+                                       output='both')
+
+    ld = LaunchDescription([joy_node])
+
+    ld.add_action(launch_ros.actions.Node(
+        package='joy_teleop', executable='joy_teleop',
+        parameters=[param]))
+
+    return ld
